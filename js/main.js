c2d77be54b5c07cdc3f1fd4f3bb9e32d91b1146f
Matt Fannin <mattfannin@acidic.co.nz>
2016-02-24 21:38:15 +1300

    fixed scroll calculation / navigation active class bug

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // Initialise Smooth Scroll and Gumshoe for Navigation
-smoothScroll.init();
-gumshoe.init({
-    offset: 2, // Distance in pixels to offset calculations
+smoothScroll.init({
+    speed: 560, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeInOutCubic', // Easing pattern to use
+    offset: -1, // Integer. How far to offset the scrolling anchor location in pixels
+    updateURL: true, // Boolean. If true, update the URL hash on scroll
 });
+gumshoe.init();

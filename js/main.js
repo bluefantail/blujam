7d3828b6bfe38775207041b7720cb652af7caafa
Matt Fannin <mattfannin@acidic.co.nz>
2016-02-25 00:27:57 +1300

    Possible fix for nav active class bug, switched order of calls to smooth scroll and gumtree

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Initialise Smooth Scroll and Gumshoe for Navigation
+gumshoe.init();
 smoothScroll.init({
     speed: 560, // Integer. How fast to complete the scroll in milliseconds
     easing: 'easeInOutCubic', // Easing pattern to use
     offset: -1, // Integer. How far to offset the scrolling anchor location in pixels
     updateURL: true, // Boolean. If true, update the URL hash on scroll
 });
-gumshoe.init();

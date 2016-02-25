a193d4db958dab4299cb1bad5a7d56eaef23c174
Ben Anderson <ben.anderson@powershop.co.nz>
2016-02-26 10:18:09 +1300

    Add unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,23 @@
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "/apps/blujm/current"
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "/apps/blujm/shared/unicorn.pid"
+
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/apps/blujm/shared/log/unicorn.log"
+stdout_path "/apps/blujm/shared/log/unicorn.log"
+
+# Unicorn socket
+listen "/apps/blujm/shared/unisock.sock"
+
+# Number of processes
+# worker_processes 4
+worker_processes 2
+
+# Time-out
+timeout 30

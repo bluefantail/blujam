829db49e7421ba90fe22ae4b821a91c0e62caffb
Matt Fannin <mattfannin@acidic.co.nz>
2016-02-26 12:43:37 +1300

    Contact form back to correct email

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -56,8 +56,8 @@ function handle_contact(event){
 
 	// Only jquery dep
 	$.ajax({
-    	url: "//formspree.io/mattfannin@acidic.co.nz", 
-    	// url: "//formspree.io/joshua.scott.132@gmail.com", 
+    	// url: "//formspree.io/mattfannin@acidic.co.nz", 
+    	url: "//formspree.io/joshua.scott.132@gmail.com", 
     	method: "POST",
     	data: $('#contact-form').serialize(),
     	dataType: "json"

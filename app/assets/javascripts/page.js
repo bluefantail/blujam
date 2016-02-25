ccdb4547c67f77fd16a1fb58fdad8527ff0968ec
Matt Fannin <mattfannin@acidic.co.nz>
2016-02-26 11:34:39 +1300

    Finished splitting content out to partials

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -56,7 +56,7 @@ function handle_contact(event){
 
 	// Only jquery dep
 	$.ajax({
-    	url: "//formspree.io/mattfannin@acidic.co.nz", 
+    	url: "//formspree.io/joshua.scott.132@gmail.com", 
     	method: "POST",
     	data: $('#contact-form').serialize(),
     	dataType: "json"

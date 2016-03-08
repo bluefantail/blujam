abca0e38b55fc74696bbffa746c7eff597af3f08
Ben Anderson <ben.anderson@powershop.co.nz>
2016-03-08 16:22:22 +1300

    Update deploy options

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -7,6 +7,9 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
+server 'sodalite.acidic.co.nz', roles: %w{app db web}
+set :deploy_to, '/apps/blujm-beta'
+set :environment, 'test'
 
 
 # role-based syntax

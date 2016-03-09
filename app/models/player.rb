65f67e928271171901dae1cb4f3435e076520b29
Ben Anderson <ben.anderson@powershop.co.nz>
2016-03-09 21:20:59 +1300

    Improve error messages, slightly

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ActiveRecord::Base
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, message: "This email is already registered"
   
   validates_presence_of :food, :vec, :name
   
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ActiveRecord::Base
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, message: "Sorry, but that team name is already taken"
   
   has_many :players
 end

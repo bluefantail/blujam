46bcb875a373851de9269b3bdcd34207f0f3d3e5
Ben Anderson <ben.anderson@powershop.co.nz>
2016-03-11 21:38:35 +1300

    Slight fix to the deploy recipe to prevent db-unlinking during test deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :tmp_dir, "/tmp/#{`whoami`.split('\\').first.chomp}"
 set :deploy_to, '/apps/blujm'
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'db/production.sqlite3')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'db/production.sqlite3', 'db/test.sqlite3')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')

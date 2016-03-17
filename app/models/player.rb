53e9d80b968791dc04a323a931a865b0d61ab986
Ben Anderson <ben.anderson@powershop.co.nz>
2016-03-18 11:19:39 +1300

    Prevent duplicate email singups

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ActiveRecord::Base
-  validates_uniqueness_of :email, message: "This email is already registered: #{email}"
+  validates_uniqueness_of :email, message: "is already registered: %{value}"
   
   validates_presence_of :food, :vec, :name
   

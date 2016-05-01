9c83fe85f593ad500e8a57561909a75940183d9f
Ben Anderson <ben.anderson@powershop.co.nz>
2016-05-01 13:17:32 +1200

    Add countdown timer, add contest closing

---
+++ app/controllers/admin/contests_controller.rb
@@ -0,0 +1,22 @@
+class Admin::ContestsController < ApplicationController
+  def index
+    @contests = Contest.all
+  end
+  
+  def show
+    @contest = Contest.find(params[:id])
+  end
+  
+  def new
+    contest = Contest.create!
+    
+    redirect_to admin_contests_path
+  end
+  
+  def destroy
+    contest = Contest.find(params[:id])
+    contest.destroy!
+    
+    redirect_to admin_contests_path
+  end
+end
